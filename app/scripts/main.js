30ccd8489771aa375ba18429f54bf0037091adf4
Roman Krejcik <farin@farin.cz>
2014-11-28 14:28:46 +0100

    avatar rendered with React

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,20 @@
 /** @jsx React.DOM */
+
+var AvatarDesigner = React.createClass({
+  render: function() {
+    return (
+      <svg width="400" height="400">
+        <rect width="200" height="200" x="100" y="200" fill="OliveDrab"></rect>
+        <circle r="100" cx="200" cy="120" fill="PapayaWhip"></circle>
+        <circle r="20" cx="160" cy="100" fill="black"></circle>
+        <circle r="20" cx="240" cy="100" fill="black"></circle>
+        <rect width="50" height="10" x="175" y="160" fill="black"></rect>
+      </svg>
+    );
+  }
+});
+
+React.render(
+  <AvatarDesigner/>,
+  document.getElementById('avatar-designer')
+);
