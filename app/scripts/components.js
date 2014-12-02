2818ea65ea904015f3b63e7027fb03e1903ba806
Roman Krejcik <farin@farin.cz>
2014-12-02 14:27:28 +0100

    form control mixin

--- app/scripts/components.js
+++ app/scripts/components.js
@@ -1,27 +1,34 @@
 /** @jsx React.DOM */
 
-window.SelectWidget = React.createClass({
+var FormControlMixin = {
   render: function() {
     return (
       <div className="form-group">
         <label>{this.props.label}</label>
-        <select className="form-control" defaultValue={this.props.defaultValue} onChange={this.props.onChange}>
-          {this.props.options.map(function(color) {
-            return (<option key={color.value} value={color.value}>{color.name}</option>);
-          })}
-        </select>
+        {this.renderFormControl()}
       </div>
+    );
+  }
+};
+
+window.SelectWidget = React.createClass({
+  mixins: [FormControlMixin],
+
+  renderFormControl: function() {
+    return (
+      <select className="form-control" defaultValue={this.props.defaultValue} onChange={this.props.onChange}>
+        {this.props.options.map(function(color) {
+          return (<option key={color.value} value={color.value}>{color.name}</option>);
+        })}
+      </select>
     );
   }
 });
 
 window.InputWidget = React.createClass({
-  render: function() {
-    return (
-      <div className="form-group">
-        <label>{this.props.label}</label>
-        <input className="form-control" defaultValue={this.props.defaultValue} onChange={this.props.onChange}/>
-      </div>
-    );
+  mixins: [FormControlMixin],
+
+  renderFormControl: function() {
+    return <input className="form-control" defaultValue={this.props.defaultValue} onChange={this.props.onChange}/>;
   }
 });
